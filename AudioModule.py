049e6da71aa5f317b02d3a4558e78a9a5ecb8008
Hans-Ulrich Berendes <berendhs@iis.fraunhofer.de>
2021-04-28 16:44:43 +0200

    Remodeled stm and data handling

--- AudioModule.py
+++ AudioModule.py
@@ -22,37 +22,38 @@ class Recorder:
         self.channels = 2
         self.fs = 44100  # Record at 44100 samples per second
         self.p = pyaudio.PyAudio()
-        self.latest_samples_object = []
-        self.dummy_dummy = [0,1,2,3]
+        self.frames = []
+        self.record_running = False
 
     def record(self):
         print("Recording...")
+        self.finished_recording = True
         stream = self.p.open(format=self.sample_format,
                 channels=self.channels,
                 rate=self.fs,
                 frames_per_buffer=self.chunk,
                 input=True)
-        frames = []  # Initialize array to store frames
         self.dummy_dummy = [7,8,9]
         self.recording = True
+        self.frames = []
         while self.recording:
             data = stream.read(CHUNK)
-            frames.append(data)
+            self.frames.append(data)
         print("done recording")
         # Stop and close the stream
-        latest_samples = frames
-        self.latest_samples_object = frames
         stream.stop_stream()
         stream.close()
         # Terminate the PortAudio interface
         self.p.terminate()
         
-    def stop(self):
+        
+    def stop_recording(self):
         print("stop")
         self.recording = False
 
+
     def getFrames(self):
-        return self.latest_samples_object
+        return self.frames
 
 """
 Helper class for recording Audio. 
@@ -63,23 +64,22 @@ Example code on how to use it:
 
 """
 
-def process_audio(data):
+def process_audio(data, filename):
         print("Processing")
         # Save the recorded data as a WAV file
-        wf = wave.open(FILENAME, 'wb')
+        wf = wave.open(filename, 'wb')
         wf.setnchannels(CHANNELS)
         p = pyaudio.PyAudio()
         wf.setsampwidth(p.get_sample_size(pyaudio.paInt16))
         wf.setframerate(SAMPLEFREQUENCY)
-        wf.writeframes(b''.join(data))
+        wf.writeframes(data)
         wf.close()
         print('Done processing')
 
         
 class Player:
     def __init__(self):
-        pass
-        
+        self.playing = True        
     def play(self, filename): 
         print("Playing the audio ")
         # Open the sound file 
@@ -97,18 +97,19 @@ class Player:
 
         # Read data in chunks
         data = wf.readframes(CHUNK)
-        stop_playing = False
+        self.playing = True
         # Play the sound by writing the audio data to the stream
-        while data != '' or self.stop_playing:
+        while (data != b'') and self.playing:
             stream.write(data)
             data = wf.readframes(CHUNK)
+            #print('playing...')
         print('done playing audio')
         # Close and terminate the stream
         stream.close()
         p.terminate()
 
     def stop_playing():
-        self.stop_playing = True
+        self.playing = False
 
 
 class AudioHelper:
@@ -124,7 +125,7 @@ class AudioHelper:
         s_ready = {'name': 'ready'}
         s_playing = {'name': 'playing', 'do': 'play(*)'}
 
-        self.stm_player = Machine(name='stm_player', transitions=[t0_p, t1_p, t2_p], states=[s_playing, s_ready], obj=self.player)
+        self.stm_player = Machine(name='stm_player', transitions=[t0_p, t1_p, t2_p, t3_p], states=[s_playing, s_ready], obj=self.player)
         self.player.stm = self.stm_player
 
 
@@ -132,20 +133,18 @@ class AudioHelper:
                 
         t0_r = {'source': 'initial', 'target': 'ready'}
         t1_r = {'trigger': 'start_recording', 'source': 'ready', 'target': 'recording'}
-        t2_r = {'trigger': 'stop_recording', 'source': 'recording', 'target': 'stopped_recording', 'effect': 'stop()'}
-        t3_r = {'trigger' : 'done', 'source' : 'stopped_recording', 'target' : 'ready'}
+        t2_r = {'trigger': 'done', 'source': 'recording', 'target': 'ready'}
+        #t3_r = {'trigger' : 'done', 'source' : 'stopped_recording', 'target' : 'ready'}
         s_ready = {'name': 'ready'}
-        s_recording = {'name': 'recording', 'do': 'record()'}
-        s_stopped_recording = {'name': 'stopped_recording'}
-        self.stm_recording = Machine(name='stm_recording', transitions=[t0_r, t1_r, t2_r, t3_r], states=[s_ready, s_recording, s_stopped_recording], obj=self.recorder)
+        s_recording = {'name': 'recording', 'do': 'record()', "stop" : "stop_recording()"}
+        #s_stopped_recording = {'name': 'stopped_recording'}
+        self.stm_recording = Machine(name='stm_recording', transitions=[t0_r, t1_r, t2_r], states=[s_ready, s_recording], obj=self.recorder)
         self.recorder.stm = self.stm_recording
         self.driver = Driver()
         self.driver.add_machine(self.stm_recording)
         self.driver.add_machine(self.stm_player)
         print('Driver created')
         self.driver.start()
-
-        self.last_record = []
     
     def play_audio(self, filename):
         print("driver started")
@@ -158,10 +157,11 @@ class AudioHelper:
     
     def start_recording(self):
         print("driver started")
-        self.driver.send('start_recording', 'stm_recording')
+        self.last_record = []
+        self.stm_recording.send('start_recording')
         
     def stop_recording(self):
-        self.driver.send('stop_recording', 'stm_recording')
+        self.stm_recording.send("stop")
 
     def get_filename(self):
         #Returns the filename that is used for temp storage
@@ -170,7 +170,7 @@ class AudioHelper:
     def get_recorded_samples(self):
         stms = []
         #wtf
-        for stm_id in self.driver._stms_by_id:
-            stms.append(self.driver._stms_by_id[stm_id])
+        #for stm_id in self.driver._stms_by_id:
+        #    stms.append(self.driver._stms_by_id[stm_id])
         #return stms[0]._obj.getFrames()
-        return []
+        return b''.join(self.stm_recording._obj.getFrames())
